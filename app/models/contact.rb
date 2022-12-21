db5f85c027f9f3a77af2652eca70a33f65ddd36e
lester wee <lester.wee.68@gmail.com>
2022-12-22 02:09:31 +0800

    added email_validator gemfile. wrote and tested validations for Project and Contact models.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,9 @@
 class Contact < ApplicationRecord
+  validates :name, :email, :company, :country, :help, :marketing, presence: true
+
+  validates :help, inclusion: { in: %w[employment freelance projects startups] }
+
+  validates :marketing, inclusion: { in: %w[google linkedin nodeflair lewagon] }
+
+  validates :email, email: true
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,5 @@
 class Project < ApplicationRecord
+  validates :name, presence: true
+  validates :link, presence: true
+  validates :description, presence: true
 end
