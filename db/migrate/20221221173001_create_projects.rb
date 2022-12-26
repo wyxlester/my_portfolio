8c532269109272a1c9c50cd93d9baa7866e0ca7d
lester wee <lester.wee.68@gmail.com>
2022-12-26 18:29:51 +0800

    added navbar template from Le Wagon UI Kit with correct labels, less personal brand logo tbc.

--- db/migrate/20221221173001_create_projects.rb
+++ db/migrate/20221221173001_create_projects.rb
@@ -1,8 +1,8 @@
 class CreateProjects < ActiveRecord::Migration[7.0]
   def change
     create_table :projects do |t|
-      t.string :name
-      t.string :link
+      t.string :name, null: false
+      t.string :link, null: false
       t.text :description
 
       t.timestamps
